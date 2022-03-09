76f1fd3d155754a5e2a6488832d3eeeada16216f
Vimalarun <arunachalamvimal0@gmail.com>
2022-03-09 18:29:43 +0530

    some changes

---
+++ hashmap-implementation/sample.java
@@ -0,0 +1,18 @@
+import customhashmap.*;
+
+public class sample {
+    public static void main(String[] args) {
+        customHashMap<Integer,String> cs = new customHashMap<>();
+        cs.put(10,"John");
+        cs.put(11,"Jack");
+        cs.put(12,"Jack");
+        cs.put(13,"Jack");
+        cs.put(14,"Jack");
+        cs.put(15,"Jack");
+        cs.put(106,"Jack");
+        cs.put(107,"Jack");
+        cs.put(108,"Jack");
+        cs.put(809,"Jack");
+        // cs.display();
+    }
+}
